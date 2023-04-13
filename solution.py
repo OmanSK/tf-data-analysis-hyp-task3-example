a1585e5d8a8b9302c326f4468b25544c26d7fa4e
OmanSK <110732673+OmanSK@users.noreply.github.com>
2023-04-13 22:56:08 +0300

    Update solution.py

--- solution.py
+++ solution.py
@@ -1,18 +1,18 @@
 import pandas as pd
 import numpy as np
-from scipy.stats import ttest_ind
+from statsmodels.stats.weightstats import ztest as z_test
 
 
 chat_id = 143893840 # Ваш chat ID, не меняйте название переменной
 
-def solution(args) -> bool: # Одна или две выборке на входе, заполняется исходя из условия
+def solution(*args) -> bool: # Одна или две выборке на входе, заполняется исходя из условия
     a, *b = args
     if not b:
         b = a
     
     p_value = 0.1
-    result = ttest_ind(a, b, equal_var=False)
+    result = z_test(a, b)
     
     
     
-    return result.pvalue < result
+    return result[-1] < result
